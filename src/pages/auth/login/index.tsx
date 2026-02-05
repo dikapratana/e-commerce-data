8c866481603f544103d3e88bd1f8e6e9fad44f1a
dikapratana <dikapratana11@gmail.com>
2026-02-05 16:18:08 +0700

    add bg

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,16 +1,14 @@
 import { Flex } from "antd";
-import { IMAGES } from "../../../configs/constants/images";
 import LoginFragment from "../../../fragments/login";
 
 export default function LoginPage() {
   return (
     <Flex
-      className="h-screen bg-white relative bg-cover bg-center"
+      className="h-screen bg-linear-to-br from-[#3A6CA5] to-[#355E88] relative"
       justify="center"
       align="center"
-      style={{ backgroundImage: `url(${IMAGES.BgLogin})` }}
     >
-      <LoginFragment/>
+      <LoginFragment />
     </Flex>
   );
 }
